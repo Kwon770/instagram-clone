b7872361a01153f55cbcb32bf4bafb5d3971460a
Kwon770 <sckwon770@gmail.com>
2020-03-25 11:10:33 +0900

    3-10 follow unfollow resolver;

--- src/api/User/confirmSecret/confirmSecret.js
+++ src/api/User/confirmSecret/confirmSecret.js
@@ -7,6 +7,12 @@ export default {
       const { email, secret } = args;
       const user = await prisma.user({ email });
       if (user.loginSecret == secret) {
+        await prisma.updateUser({
+          where: { id: user.id },
+          data: {
+            loginSecret: ""
+          }
+        });
         // JWT Tokent
         return GenerateToken(user.id);
       } else {
