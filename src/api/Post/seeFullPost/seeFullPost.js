2ffab0127447f4301a8a055c35faee3aed6bb8d8
Kwon770 <sckwon770@gmail.com>
2020-04-06 14:26:12 +0900

    ...

--- src/api/Post/seeFullPost/seeFullPost.js
+++ src/api/Post/seeFullPost/seeFullPost.js
@@ -1,9 +1,9 @@
 import { prisma } from "../../../../generated/prisma-client";
-import { COMMENT_FRAGMENT } from "../../../fragments";
+import { FULL_POST_FRAGMENT } from "../../../fragments";
 
 export default {
   Query: {
-    seeFullPost: async (_, args) => {
+    seeFullPost: (_, args) => {
       const { id } = args;
       const post = await prisma.post({ id });
       const comments = await prisma
@@ -14,15 +14,11 @@ export default {
         .likesConnection({ where: { post: { id } } })
         .aggregate()
         .count();
-      const files = await prisma.post({ id }).files();
-      const user = await prisma.post({ id }).user();
       return {
         post,
         comments,
-        likeCount,
-        files,
-        user,
+        likeCount
       };
-    },
-  },
+    }
+  }
 };
