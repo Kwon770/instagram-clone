60a9dcf9c0a8eeb24cd64d6c4eb31f11cff8ec2b
Kwon770 <sckwon770@gmail.com>
2020-04-06 14:20:10 +0900

    3-19 seeFullPost Refactoring [BROKEN]

--- src/fragments.js
+++ src/fragments.js
@@ -1,9 +1,34 @@
 export const COMMENT_FRAGMENT = `
-    fragment CommentParts on Comment {
         id
         text
         user {
-            userName
+            ${USER_FRAGMENT}
+        }
+`;
+
+export const USER_FRAGMENT = `
+        id
+        userName
+`;
+
+export const FILE_FRAGMENT = `
+        id
+        url
+`;
+
+export const FULL_POST_FRAGMENT = `
+    fragment PostParts on Post{
+        id
+        location
+        caption
+        files {
+            ${FILE_FRAGMENT}
+        }
+        comments {
+            ${COMMENT_FRAGMENT}
+        }
+        user {
+            ${USER_FRAGMENT}
         }
     }
 `;
