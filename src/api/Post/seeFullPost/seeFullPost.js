7f65a9347347dda3b63c54e449793e72f7bd761f
Kwon770 <sckwon770@gmail.com>
2020-04-07 10:52:49 +0900

    3-19 seeFullPost Refactoring [FIXED]

--- src/api/Post/seeFullPost/seeFullPost.js
+++ src/api/Post/seeFullPost/seeFullPost.js
@@ -5,26 +5,7 @@ export default {
   Query: {
     seeFullPost: (_, args) => {
       const { id } = args;
-<<<<<<< HEAD
-      const post = await prisma.post({ id });
-      const comments = await prisma
-        .post({ id })
-        .comments()
-        .$fragment(COMMENT_FRAGMENT);
-      const likeCount = await prisma
-        .likesConnection({ where: { post: { id } } })
-        .aggregate()
-        .count();
-      return {
-        post,
-        comments,
-        likeCount
-      };
-    }
-  }
-=======
       return prisma.post({ id }).$fragment(FULL_POST_FRAGMENT);
     },
   },
->>>>>>> 60a9dcf9c0a8eeb24cd64d6c4eb31f11cff8ec2b
 };
