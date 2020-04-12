9f92427f9146249a471c64ae96e1766b5a2b8de9
Kwon770 <sckwon770@gmail.com>
2020-04-13 08:02:36 +0900

    3-28 newMessage Subscription part Two

---
+++ src/api/Message/newMessage/newMessage.js
@@ -0,0 +1,15 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Subscription: {
+    newMessage: {
+      subscribe: (_, args) => {
+        const { roomId } = args;
+        return prisma.$subscribe.message({
+          AND: [{ mutation_in: "CREATED" }, { node: { room: { id: roomId } } }]
+        });
+      },
+      resolve: payload => payload
+    }
+  }
+};
