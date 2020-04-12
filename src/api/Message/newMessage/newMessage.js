482e8ba40ac1378ef9b024a0a6bd4f5f021be495
Kwon770 <sckwon770@gmail.com>
2020-04-13 08:10:54 +0900

    3-29 conclusions

--- src/api/Message/newMessage/newMessage.js
+++ src/api/Message/newMessage/newMessage.js
@@ -5,9 +5,14 @@ export default {
     newMessage: {
       subscribe: (_, args) => {
         const { roomId } = args;
-        return prisma.$subscribe.message({
-          AND: [{ mutation_in: "CREATED" }, { node: { room: { id: roomId } } }]
-        });
+        return prisma.$subscribe
+          .message({
+            AND: [
+              { mutation_in: "CREATED" },
+              { node: { room: { id: roomId } } }
+            ]
+          })
+          .node();
       },
       resolve: payload => payload
     }
